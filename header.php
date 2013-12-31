b3c04c9db979080020d471cc076aba3dd3d1fe59
caslay <egystory@me.com>
2013-12-31 02:26:14 +0200

    Meta Title Fix

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="utf-8">
-    <title><?php wp_title(' - ', true, 'left'); ?></title>
+    <title><<?php blog_info('title') ?><?php wp_title(' - ', true, 'left'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?php bloginfo('description'); ?>">
     <meta name="author" content="<?php bloginfo('name'); ?>">
