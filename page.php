a050540fd7dafdb03df2ea44231cbe09832d9a96
Hugo Ferreira <hugo@mindclick.info>
2014-04-02 00:41:36 +0100

    Also populate the sharing links on pages

--- page.php
+++ page.php
@@ -12,27 +12,27 @@
 				<div class="share">
 					<ul>
 						<li>
-							<a href="#" title="share on facebook">
+							<a target="_blank" href="http://www.facebook.com/share.php?u=<?php print(urlencode(get_permalink())); ?>&title=<?php print(urlencode(the_title())); ?>" title="share on facebook">
 								<i class="icon-facebook"></i>
 							</a>
 						</li>
 						<li>
-							<a href="#" title="Tweet">
+							<a target="_blank" href="http://twitter.com/home?status=<?php print(urlencode(get_permalink())); ?>+<?php print(urlencode(the_title())); ?>">
 								<i class="icon-twitter"></i>
 							</a>
 						</li>
 						<li>
-							<a href="#" title="Post on Google plus">
+							<a target="_blank" href="https://plus.google.com/share?url=<?php print(urlencode(get_permalink())); ?>" title="Post on Google plus">
 								<i class="icon-google-plus"></i>
 							</a>
 						</li>
 						<li>
-							<a href="#" title="Post on Linkedin">
+							<a target="_blank" href="http://www.linkedin.com/shareArticle?mini=true&url=<?php print(urlencode(get_permalink())); ?>&title=<?php print(urlencode(the_title())); ?>&source=[SOURCE/DOMAIN]" title="Post on Linkedin">
 								<i class="icon-linkedin"></i>
 							</a>
 						</li>
 						<li>
-							<a href="#" title="Pin it">
+							<a target="_blank" href="http://pinterest.com/pin/create/bookmarklet/?media=[MEDIA]&url=<?php print(urlencode(get_permalink())); ?>&is_video=false&description=<?php print(urlencode(the_title())); ?>" title="Pin it">
 								<i class="icon-pinterest"></i>
 							</a>
 						</li>
